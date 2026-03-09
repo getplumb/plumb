2a98c6a029d96e882e548d55abd472161a794aa0
Clay Waters <claywaters13@gmail.com>
2026-03-09 13:46:36 -0600

    chore: add MIT license, fix hardcoded paths in wipe-db and task doc

--- wipe-db.ts
+++ wipe-db.ts
@@ -1,6 +1,8 @@
 import { existsSync, unlinkSync } from 'fs';
+import { homedir } from 'os';
+import { join } from 'path';
 
-const DB_PATH = '/home/openclaw-host/.plumb/memory.db';
+const DB_PATH = process.env.PLUMB_DB_PATH ?? join(homedir(), '.plumb', 'memory.db');
 
 if (existsSync(DB_PATH)) {
   unlinkSync(DB_PATH);
